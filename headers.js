c2da2d949f38a13e1b22d3b93e7ec842bff44d72
Gary Taylor <StrictDev@Garys-MacBook-Pro.local>
2016-09-17 18:57:16 -0700

    madame already ready for public

--- headers.js
+++ headers.js
@@ -3,5 +3,4 @@ var http_1 = require('@angular/http');
 exports.contentHeaders = new http_1.Headers();
 exports.contentHeaders.append('Accept', 'application/json');
 exports.contentHeaders.append('Content-Type', 'application/json');
-exports.contentHeaders.append('X-AuthToken', 'MyTokenBitch');
 //# sourceMappingURL=headers.js.map
--- ts/headers.ts
+++ ts/headers.ts
@@ -3,4 +3,4 @@ import { Headers } from '@angular/http';
 export const contentHeaders = new Headers();
 contentHeaders.append('Accept', 'application/json');
 contentHeaders.append('Content-Type', 'application/json');
-contentHeaders.append('X-AuthToken', 'MyTokenBitch');
+// contentHeaders.append('X-AuthToken', 'ExtraTokens');
